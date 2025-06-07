c3908c3ab84558abedd2df30deee62188c285ddd
msmi1403 <malcolmdsmith1@gmail.com>
2025-06-07 00:39:43 -0700

    Replace old meal plan generator with simplified version

--- saucey-cloud-functions/mealPlanFunctions/handlers/generateMealPlan.handler.js
+++ saucey-cloud-functions/mealPlanFunctions/handlers/generateMealPlan.handler.js
@@ -452,11 +452,19 @@ function getWeekAlignedStartDate(date) {
 }
 
 function generatePlanId() {
-  return `plan_${Date.now()}_${Math.random().toString(36).substr(2, 9)}`;
+  // Generate UUID-style ID compatible with Swift UUID
+  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+    var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+    return v.toString(16);
+  });
 }
 
 function generateItemId() {
-  return `item_${Date.now()}_${Math.random().toString(36).substr(2, 9)}`;
+  // Generate UUID-style ID compatible with Swift UUID
+  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+    var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+    return v.toString(16);
+  });
 }
 
 module.exports = { generateMealPlan }; 
